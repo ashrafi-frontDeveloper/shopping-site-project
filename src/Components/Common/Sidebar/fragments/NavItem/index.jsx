ccc70a3132f635f656e4c82b0fa6266725a1cd75
Mohammad Ashrafi <ashrafi.dev221184@gmail.com>
2026-08-06 19:54:18 +0330

    add Interacive NavItem

--- src/Components/Common/Sidebar/fragments/NavItem/index.jsx
+++ src/Components/Common/Sidebar/fragments/NavItem/index.jsx
@@ -8,12 +8,32 @@ const index = ({ label = "", href, icon, bgColor, iconColor }) => {
         "flex items-center gap-3 duration-150 transition-all hover:bg-zinc-100/70 p-1.5 rounded-lg relative"
       }
     >
-      <div className={`size-10 rounded-lg flex items-center justify-center text-xl relative overflow-hidden ${iconColor}`}>
-        <span className={`size-full absolute z-0 opacity-15 inset-0 ${bgColor}`}></span>
-        {icon}
-      </div>
+      {(link) => (
+        <>
+          {link.isActive && (
+            <>
+              <div
+                className={`absolute size-full right-0 rounded-lg opacity-6 ${bgColor}`}
+              ></div>
 
-      <span className="text-sm text-zinc-600 ">{label}</span>
+              <span
+                className={`absolute top-0 w-1 rounded-full h-[80%] bottom-0 my-auto -right-1 z-0 opacity-50 ${bgColor}`}
+              ></span>
+            </>
+          )}
+
+          <div
+            className={`size-10 rounded-lg flex items-center justify-center text-xl relative overflow-hidden ${iconColor}`}
+          >
+            <span
+              className={`size-full absolute z-0 opacity-15 inset-0 ${bgColor}`}
+            ></span>
+            {icon}
+          </div>
+
+          <span className="text-sm text-zinc-600 ">{label}</span>
+        </>
+      )}
     </NavLink>
   );
 };
